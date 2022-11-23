cd60c328dc862c468ef45cecfbe62ed98f28b5f3
MarcinLarek <marcinlarek00@gmail.com>
2022-11-23 19:24:05 +0100

    Strona główna

--- index.php
+++ index.php
@@ -5,9 +5,17 @@ require_once 'db_utils.php';
 <div class="container pt-5">
   <div class="row text-center">
     <h1>JUST IT STUFF</h1>
-    <img src="" alt="">
-    <div class="col-6">
-Witamy na naszej stronie- Just IT Stuff! Zajmujemy się sprzedażą urządzeń peryferyjnych dla najbardziej wymagających użytkowników. Chcesz być najlepszy na wirtualnym polu walki? A może wolisz całkowicie zatracić się w fantastycznym świecie? Poszukujesz sprzętu do profesjonalnych zastosowań takich jak obróbka dźwięku? To wszystko i wiele więcej znajdziesz w naszym sklepie internetowym! Zapraszamy!
+    <div class="col-3">
+
+    </div>
+    <div class="col-6 text-center">
+      <img class="img-fluid pt-3 pb-3" style="max-height: 400px" src="https://i.imgur.com/AZ8Wfv8.png" alt="">
+      <h2>Witamy na naszej stronie- Just IT Stuff!</h2>
+      <h5>Zajmujemy się sprzedażą urządzeń peryferyjnych dla najbardziej wymagających użytkowników. Chcesz być najlepszy na wirtualnym polu walki? A może wolisz całkowicie zatracić się w fantastycznym świecie? Poszukujesz sprzętu do profesjonalnych zastosowań takich jak obróbka dźwięku? To wszystko i wiele więcej znajdziesz w naszym sklepie internetowym! </h5>
+      <h2>Zapraszamy!</h2>
+    </div>
+    <div class="col-3">
+
     </div>
   </div>
 </div>
