f45589fdf8904130a1b34c13eac49b4748f4a043
MarcinLarek <marcinlarek00@gmail.com>
2022-11-24 20:52:01 +0100

    Strona główna update

--- index.php
+++ index.php
@@ -1,21 +1,41 @@
 <?php
 require_once 'db_utils.php';
 ?>
+<div class="" style="
+  background-image:url('https://i.imgur.com/9UqgkPg.jpg');background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100%;
+">
 
-<div class="container pt-5">
+</div>
+
+<div class="" style="
+  background-image:url('https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/266709904_1156417921556253_6395400610429993994_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=e3f864&_nc_ohc=45VEFC1xldIAX-4Trim&_nc_ht=scontent-waw1-1.xx&oh=00_AfAxMaUac_khF0EculTymwo_RbCwv3qb8vHNOjT2Mlz3UQ&oe=6385389C');background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100%;
+">
+
+<div class="container pt-5 pb-5">
   <div class="row text-center">
-    <h1>JUST IT STUFF</h1>
     <div class="col-3">
 
     </div>
-    <div class="col-6 text-center">
-      <img class="img-fluid pt-3 pb-3" style="max-height: 400px" src="https://i.imgur.com/AZ8Wfv8.png" alt="">
-      <h2>Witamy na naszej stronie- Just IT Stuff!</h2>
-      <h5>Zajmujemy się sprzedażą urządzeń peryferyjnych dla najbardziej wymagających użytkowników. Chcesz być najlepszy na wirtualnym polu walki? A może wolisz całkowicie zatracić się w fantastycznym świecie? Poszukujesz sprzętu do profesjonalnych zastosowań takich jak obróbka dźwięku? To wszystko i wiele więcej znajdziesz w naszym sklepie internetowym! </h5>
-      <h2>Zapraszamy!</h2>
+    <div class="pb-5">
+      <img class="pt-3 pb-3 mb-5" style="border-radius:25%; max-height: 450px" src="https://i.imgur.com/AZ8Wfv8.png" alt="">
+      <h1 class="pb-3">Witamy na naszej stronie</h1>
+      <h5>Zajmujemy się sprzedażą urządzeń peryferyjnych dla najbardziej wymagających użytkowników.</h5>
+      <h5>Chcesz być najlepszy na wirtualnym polu walki?</h5>
+      <h5>A może wolisz całkowicie zatracić się w fantastycznym świecie?</h5>
+      <h5>Poszukujesz sprzętu do profesjonalnych zastosowań takich jak obróbka dźwięku?</h5>
+      <h5>To wszystko i wiele więcej znajdziesz w naszym sklepie internetowym!</h5>
+      <h1 class="pt-3">Zapraszamy!</h2>
     </div>
     <div class="col-3">
 
     </div>
   </div>
+</div>
+
 </div>
