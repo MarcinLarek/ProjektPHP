0d980a1d66217dc95580421f5452d337aa717215
dkordylasinski <damik535@gmail.com>
2022-12-01 17:49:11 +0100

    cart

--- cart.php
+++ cart.php
@@ -15,13 +15,24 @@ if (isset($_GET['success'])) {
 }
 
 $userid = $_SESSION['ID'];
-$result = mysqli_query($db,"SELECT * FROM orders WHERE user_id = $userid");
-foreach ($result as $order) {
-  echo "W Tej pętli masz zamówienia <br>";
-  ?>
+$result = mysqli_query($db,"SELECT * FROM orders WHERE user_id = $userid"); ?>
+        <div class="cart-wrapper">
+<?php foreach ($result as $order) {
+    $prodid = $order['product_id'];
+    $product = mysqli_query($db,"SELECT * FROM products WHERE id = $prodid");
+    $singleproduct = $product->fetch_row(); ?>
 
-<!--Tutaj Pisz Htmelke-->
+            <ul>
+                <li><img src="<?php echo $singleproduct[4] ?>" alt=""></li>
+                <li><?php echo $singleproduct[1] ?></li>
+                <li><?php echo $singleproduct[3] ?>zł</li>
+                <li><?php echo $order["order_date"] ?></li>
+                <li><?php echo $order["status"] ?></li>
+            </ul>
+        <!-- <hr> -->
 
   <?php
 }
 ?>
+
+</div>
